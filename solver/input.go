5d6632e0d8765b830dedcb0e38e4e11864b0f405
Julien Klaer <klaer.j@sfeir.com>
2020-01-29 23:51:37 +0100

    Simple input reader converting lines into a dictionary for easy mapping.
    see #1, #7

---
+++ solver/input.go
@@ -0,0 +1,34 @@
+package solver
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	InputEndOfLine = '\n'
+	InputDelimiter = " "
+)
+
+func ReadInput(filename string) map[int][]string {
+	res := make(map[int][]string)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("error reading file input. got %s\n", err)
+	}
+
+	r := bufio.NewReader(f)
+	for i := 0; ; i++ {
+		l, err := r.ReadString(InputEndOfLine)
+		if err == io.EOF {
+			break
+		}
+		res[i] = strings.Split(l, InputDelimiter)
+	}
+
+	return res
+}
---
+++ solver/input_test.go
@@ -0,0 +1,15 @@
+package solver
+
+import "testing"
+
+func TestReadInput(t *testing.T) {
+	data := ReadInput("../test/a_example.in")
+	if len(data) != 2 {
+		t.Errorf("map should contain two entry because there is two line in the file. (test/a_example.in)")
+	}
+	for _, list := range data {
+		if len(list) <= 0 {
+			t.Errorf("list values from keys should not be empty.")
+		}
+	}
+}
