63fc152f9d158ab12c43c00d146680db92db9fb1
Schnikonos <nicolas.jouvet@wanadoo.fr>
2020-02-10 14:55:21 +0100

    Adding basic heap implementation as well as AStar algo (#12)
    * Adding basic heap implementation as well as AStar algo
    
    * Adding static weighting to AStar algo
    -> not sure if dynamic weighting is worth implementing
    
    * Adding method for thread safe heap

---
+++ solver/basicHeap.go
@@ -0,0 +1,39 @@
+package solver
+
+// NEED TO BE IMPLEMENTED WITH POINTER
+// ex: func (s *MyHeap) Compare(heaper BasicHeaper) {...}
+type BasicHeaper interface {
+	// if compare like this < heaper -> lowest element popped from heap
+	// if compare like this > heaper -> highest element popped first from heap
+	Compare(heaper BasicHeaper) bool
+
+	// Setter and getter for heap index in order to be able to correctly use
+	// heap.Fix -> mandatory if heap element value is changed
+	SetHeapIndex(index int)
+	GetHeapIndex() int
+}
+
+type BasicHeap []BasicHeaper
+
+func (h BasicHeap) Len() int           { return len(h) }
+func (h BasicHeap) Less(i, j int) bool { return h[i].Compare(h[j]) }
+func (h BasicHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].SetHeapIndex(i)
+	h[j].SetHeapIndex(j)
+}
+func (h *BasicHeap) Push(x interface{}) {
+	n := len(*h)
+	item := x.(BasicHeaper)
+	item.SetHeapIndex(n)
+	*h = append(*h, item)
+}
+func (h *BasicHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	old[n-1] = nil
+	x.SetHeapIndex(-1)
+	*h = old[0 : n-1]
+	return x
+}
